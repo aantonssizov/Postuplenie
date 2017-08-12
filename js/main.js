96f424149bb3afc183d18398282b99ae82d03a02
Anton Sizov <aantonssizov@gmail.com>
2017-08-12 14:49:28 +0300

    поработал над орфографией

--- js/main.js
+++ js/main.js
@@ -28,7 +28,8 @@ function rezult() {
     let rez = document.querySelector("#spec").value;
     let state = (document.querySelector('.filters__state').checked) ? document.querySelector('.filters__state').value = "государственный" : document.querySelector('.filters__state').value = "-";
     let private = (document.querySelector('.filters__private').checked) ? document.querySelector('.filters__private').value = "частный" : document.querySelector('.filters__private').value = "-";
-    let budget = document.getElementsByName("filters__budget")[0].value;
+    let budget = (document.querySelector(".filters__budget")[0].checked) ? document.querySelector('.filters__budget')[0].value = "yes" : document.querySelector('.filters__budget')[0].value = '-';
+
     console.log(budget);
 
     if (rez == "ERROR: Вы не выбрали профессию") {
